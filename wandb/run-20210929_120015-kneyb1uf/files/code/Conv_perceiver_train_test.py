ead23df4e0b5a19e9eeecef110a025aca3916afb
TranNhiem <tvnhiemhmus@.ncu.edu.tw>
2021-09-29 12:18:34 +0800

    commit

--- wandb/run-20210929_120015-kneyb1uf/files/code/Conv_perceiver_train_test.py
+++ wandb/run-20210929_120015-kneyb1uf/files/code/Conv_perceiver_train_test.py
@@ -5,7 +5,7 @@ from Data_utils.datasets import CIFAR100_dataset
 from tensorflow.keras import optimizers
 from tensorflow.python.keras.backend import dropout, learning_phase
 import tensorflow_addons as tfa
-from Neural_Net_Architecture.Convnet_Transformer.perceiver_compact_Conv_transformer_VIT_architecture import conv_transform_VIT
+from Neural_Net_Architecture.Convnet_Transformer.perceiver_compact_Conv_transformer_VIT_architecture import convnet_perceiver_architecture
 
 import argparse
 from tensorflow.keras.optimizers import schedules
@@ -34,20 +34,6 @@ if gpus:
 strategy = tf.distribute.MirroredStrategy()
 Auto = tf.data.experimental.AUTOTUNE
 
-
-# Setting GPUs
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-
-    try:
-        tf.config.experimental.set_visible_devices(gpus[0:8], 'GPU')
-        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
-    except RuntimeError as e:
-        print(e)
-
-strategy = tf.distribute.MirroredStrategy()
-
 # Try to keep latten array small
 input_shape = (32, 32, 3)
 IMG_SIZE = 32
@@ -59,10 +45,10 @@ num_class = 100
 
 num_conv_layers = 2  # for unroll patches -- Overlap
 spatial2projection_dim = [128, 256]  # This equivalent to # filters
-position_embedding_option = True
+conv_position_embedding = True
 latten_dim = 128  # size of latten array --> (N)
 projection_dim = 256
-dropout_rate = 0.2
+dropout = 0.2
 stochastic_depth_rate = 0.1
 # Learnable array
 # (NxD) #--> OUTPUT( [Q, K][Conetent information, positional])
@@ -77,57 +63,76 @@ NUM_MODEL_LAYERS = 4
 
 # 2 layer MLP Dense with number of Unit= pro_dim
 FFN_layers_units = [projection_dim, projection_dim]
-classification_unit = [projection_dim, num_class]
+classification_head = [projection_dim, num_class]
 
 print(f"Image size: {IMG_SIZE} X {IMG_SIZE} = {IMG_SIZE ** 2}")
 # print(f"Patch size: {patch_size} X {patch_size} = {patch_size ** 2} ")
 # print(f"Patches per image: {num_patches}")
 # print(
 #     f"Elements per patch [patch_size*patch_size] (3 channels RGB): {(patch_size ** 2) * 3}")
-
+print(f"Latent array shape: {latten_dim} X {projection_dim}")
 # print(f"Data array shape: {num_patches} X {projection_dim}")
-
+args = parse_args()
+BATCH_SIZE = args.train_batch_size
+BATCH_SIZE = BATCH_SIZE * strategy.num_replicas_in_sync
+print("Global _batch_size", BATCH_SIZE)
 
 with strategy.scope():
 
     def main(args):
 
-        BATCH_SIZE = args.train_batch_size
         EPOCHS = args.train_epochs
 
         # Prepare data training
         data = CIFAR100_dataset(BATCH_SIZE, IMG_SIZE)
         num_images = data.num_train_images
-        train_ds, test_ds = data.supervised_train_ds_test_ds()
 
+        def dataset_fn(input_context):
+            batch_size = input_context.get_per_replica_batch_size(
+                BATCH_SIZE)
+            train_ds, test_ds = data.supervised_train_ds_test_ds()
+            train_ds = (train_ds.shard(input_context.num_input_pipelines,
+                                       input_context.input_pipeline_id)
+                        .batch(batch_size)
+                        .prefetch(Auto)
+                        )
+            test_ds = (test_ds.shard(input_context.num_input_pipelines,
+                                     input_context.input_pipeline_id)
+                       .batch(batch_size)
+                       .prefetch(Auto)
+                       )
+
+            return train_ds, test_ds
+        # train_dataset=strategy.experimental_distribute_dataset(train_dataset)
+        # train_ds, test_ds = strategy.experimental_distribute_datasets_from_function(
+        #     lambda input_context: dataset_fn(input_context))
+
+        train_ds, test_ds = data.supervised_train_ds_test_ds()
+        # train_ds = strategy.experimental_distribute_dataset(train_ds)
+        # test_ds = strategy.experimental_distribute_dataset(test_ds)
         # Create model Architecutre
         # Noted of Input pooling mode 2D not support in current desing ["1D","sequence_pooling" ]
+        conv_perceiver_model = convnet_perceiver_architecture(IMG_SIZE, num_conv_layers,  conv_position_embedding, spatial2projection_dim,
+                                                              latten_dim, projection_dim, num_multi_heads,
+                                                              NUM_TRANSFORMER_BLOCK, NUM_MODEL_LAYERS, FFN_layers_units, dropout,
+                                                              classification_head, include_top=True, pooling_mode="1D",
+                                                              stochastic_depth=False, stochastic_depth_rate=stochastic_depth_rate)
 
-        conv_VIT_model = conv_transform_VIT(num_class, IMG_SIZE, num_conv_layers, spatial2projection_dim, position_embedding_option, projection_dim,
-                                            NUM_TRANSFORMER_BLOCK, num_multi_heads,
-                                            FFN_layers_units, classification_unit, dropout_rate,
-                                            stochastic_depth=False, stochastic_depth_rate=stochastic_depth_rate,
-                                            include_top=True, pooling_mode="sequence_pooling",
-                                            )
-
-        conv_VIT_model(tf.keras.Input((input_shape)))
-        conv_VIT_model.summary()
+        conv_perceiver_model(tf.keras.Input((input_shape)))
+        conv_perceiver_model.summary()
 
         # Initialize the Random weight
         x = tf.random.normal((BATCH_SIZE, IMG_SIZE, IMG_SIZE, 3))
-        h = conv_VIT_model(x, training=False)
+        h = conv_perceiver_model(x, training=False)
         print("Succeed Initialize online encoder")
         print(f"Conv_Perciever encoder OUTPUT: {h.shape}")
 
         num_params_f = tf.reduce_sum(
-            [tf.reduce_prod(var.shape) for var in conv_VIT_model.trainable_variables])
+            [tf.reduce_prod(var.shape) for var in conv_perceiver_model.trainable_variables])
         print('The encoders have {} trainable parameters each.'.format(num_params_f))
 
-        '''
         # Configure Logs recording during training
-        
-        #Training Configure
-
+        '''Training Configure'''
         configs = {
             "Model_Arch": "Conv_Perceiver_arch",
             "DataAugmentation_types": "None for testing",
@@ -168,12 +173,15 @@ with strategy.scope():
 
         # 3. Schedule CosineDecay warmup
         base_lr = 0.3
-        lr_rate = WarmUpAndCosineDecay(base_lr, num_images)
-        optimizers = get_optimizer(lr_rate)
-        AdamW = optimizers.optimizer_weight_decay
+        lr_rate = WarmUpAndCosineDecay(base_lr, num_images, args)
+        # optimizers = get_optimizer(lr_rate)
+        # AdamW = optimizers.optimizer_weight_decay(args)
+        # Borrow testing
+        optimizer = tfa.optimizers.AdamW(
+            learning_rate=lr_rate, weight_decay=args.weight_decay)
 
         # model compile
-        conv_perceiver_model.compile(optimizer=AdamW,
+        conv_perceiver_model.compile(optimizer=optimizer,
                                      loss=tf.keras.losses.CategoricalCrossentropy(),
                                      metrics=[tf.keras.metrics.CategoricalAccuracy(name="acc"),
                                               tf.keras.metrics.TopKCategoricalAccuracy(5, name="top5_acc")])
@@ -182,7 +190,6 @@ with strategy.scope():
 
         conv_perceiver_model.fit(train_ds, epochs=EPOCHS,
                                  validation_data=test_ds, callbacks=[WandbCallback()])  # callbacks=callbacks_list,
-        '''
 
     if __name__ == '__main__':
 

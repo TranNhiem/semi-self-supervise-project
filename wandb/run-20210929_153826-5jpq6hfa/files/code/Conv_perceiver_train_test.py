19e046b82d2a858665e93fa9b3b2dd8caaf53fb1
TranNhiem <tvnhiemhmus@.ncu.edu.tw>
2021-09-30 10:13:48 +0800

    commit

--- wandb/run-20210929_153826-5jpq6hfa/files/code/Conv_perceiver_train_test.py
+++ wandb/run-20210929_153826-5jpq6hfa/files/code/Conv_perceiver_train_test.py
@@ -18,9 +18,8 @@ import tensorflow as tf
 wandb.login()
 
 include_top = True
-# if include_top:
-#     # tf.config.experimental_run_functions_eagerly(True)
-#     tf.config.run_functions_eagerly(True)
+if include_top:
+    tf.config.experimental_run_functions_eagerly(True)
 
 
 # Setting GPUs
@@ -52,7 +51,7 @@ num_class = 100
 num_conv_layers = 2  # for unroll patches -- Overlap
 spatial2projection_dim = [128, 256]  # This equivalent to # filters
 conv_position_embedding = True
-latten_dim = 128  # size of latten array --> (N)
+latten_dim = 256  # size of latten array --> (N)
 projection_dim = 256
 dropout_rate = 0.2
 stochastic_depth_rate = 0.1
@@ -121,7 +120,7 @@ with strategy.scope():
         conv_perceiver_model = convnet_perceiver_architecture(IMG_SIZE, num_conv_layers,  conv_position_embedding, spatial2projection_dim,
                                                               latten_dim, projection_dim, num_multi_heads,
                                                               NUM_TRANSFORMER_BLOCK, NUM_MODEL_LAYERS, FFN_layers_units, dropout_rate,
-                                                              classification_head, include_top=include_top, pooling_mode="1D",
+                                                              classification_head, include_top=include_top, pooling_mode="sequence_pooling",
                                                               stochastic_depth=False, stochastic_depth_rate=stochastic_depth_rate)
 
         conv_perceiver_model(tf.keras.Input((input_shape)))
